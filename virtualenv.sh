34d8e05fa8437942111cc49ba7b785dc448270da
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-03-24 15:02:13 -0400

    final working script

--- virtualenv.sh
+++ virtualenv.sh
@@ -3,16 +3,17 @@
 # get the root directory
 root_dir=$(echo ~)
 
-# Script to automatically create virtual enviroment in python
+# Script to automatically create and activate virtual enviroment in python, n
+#         no deactivate option cause it is faster to just type the deactivate command.
 
-# ask the user to name the directory to activate venv
+# ask the user to name the directory to activate venv, read the input
 echo "Welcome to the virtual environment script tool, you can create(C) a virtual environment, you can activate(A) already existing virtual environment, or
  you can do nothing(N)."
 
 echo "What do you want to do, (C/A/N)?: "
-
 read action
 
+# if the user wants to activate the virtual environment that already exists
 if [ $action = 'A' ]; then
     # ask the user to name the directory to activate venv
     echo "Name of the directory to create venv in: "
@@ -23,6 +24,7 @@ if [ $action = 'A' ]; then
     dir_name="$root_dir/Documents/Docs/$dir_name"
     source $dir_name/bin/activate
 
+# if the user wants to create the virtual environment in a directory
 elif [ $action = 'C' ]; then
 
     # ask the user to name the directory to activate venv
@@ -38,15 +40,14 @@ elif [ $action = 'C' ]; then
     output=$(python3 -m venv $dir_name)
     echo "$output"
     
-    output=$(source $dir_name/bin/activate && echo "Virtual environment activated")
-    echo "$output"
+    source $dir_name/bin/activate
 
+#  if user wants to do nothing
 elif [ $action = 'N' ]; then
     echo "You did nothing."
 
+# if user is a gamer and does not read instructions
 else
     echo "Invalid input, do not be dumb. Read the instructions!!!"
 
 fi
-
-exec zsh
